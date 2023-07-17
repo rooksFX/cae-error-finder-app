5bf70a0b540682a937f14705222b71bc2841b13a
Ron Nuqui <ron_nuqui@yahoo.com>
2023-07-17 15:19:04 +0800

    Add CORS handling in the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,39 +2,17 @@ import express from 'express';
 // import fetch from 'node-fetch';
 import axios from 'axios';
 
+import cors from 'cors';
+
 const app = express();
 
-// const fetchData = async () => {
-//     const response = await fetch('https://s3.eu-west-2.amazonaws.com/interview.mock.data/payload.json');
-//     const data = await response.json();
-//     return data;
-// }
-
-const axiosGet = async() => {
-    const data = await axios({
-        method: 'GET',
-        url: 'https://s3.eu-west-2.amazonaws.com/interview.mock.data/payload.json'
-    });
-    // const data = response.data;
-    console.log('axiosGet / data: ', data);
-    return data;
-}
-
-const fetchActivities = () => {
-    axios({
-        method: 'GET',
-        url: 'https://s3.eu-west-2.amazonaws.com/interview.mock.data/payload.json'
-    }).then((response) => {
-        console.log('response: ', response.data);
-        return(response.data);
-    })
-    return {};
-}
+const PROD_CLIENT = 'https://cae-error-finder-app-client.onrender.com';
 
 app.get('/', (req, res) => {
     res.send('Hi World!');
 });
 
+app.use(cors({ origin: PROD_CLIENT }))
 app.get('/api/activities', (req, res) => {
     axios({
         method: 'GET',
